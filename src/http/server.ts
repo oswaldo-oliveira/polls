33c961973d43468d535c8f5084311bcfaf46f464
Oswaldo dos Santos <oswaldo.oliveira@compasso.com.br>
2024-02-06 20:03:24 -0300

    feat: create routes (create-poll, get-poll, vote-on-poll)

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,20 @@
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+})
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Http server running! 🚀')
